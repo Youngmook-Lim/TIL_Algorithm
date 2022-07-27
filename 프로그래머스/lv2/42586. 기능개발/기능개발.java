cf479a3c3735b59ee97c7a401fd704b266480b7b
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2022-07-28 00:46:51 +0900

    [level 2] Title: 기능개발, Time: 0.13 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        Stack<Integer> stack = new Stack<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = speeds.length - 1; i >= 0; i--) {
+            stack.push((100 - progresses[i] - 1) / speeds[i] + 1);
+//            if ((100 - progresses[i]) % speeds[i] == 0)
+//                stack.push((100 - progresses[i]) / speeds[i]);
+//            else
+//                stack.push((100 - progresses[i]) / speeds[i] + 1);
+        }
+        while (!stack.isEmpty()) {
+            int cnt = 1;
+            int a = stack.pop();
+
+            while (!stack.isEmpty() && a >= stack.peek()) {
+
+                stack.pop();
+                cnt++;
+            }
+
+            list.add(cnt);
+        }
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++)
+            answer[i] = list.get(i);
+        return answer;
+    }
+}
