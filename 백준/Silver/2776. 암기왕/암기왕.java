95093406a9fd401f072eecdacd2bfaef07c6afbe
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2022-08-01 22:45:49 +0900

    [Silver IV] Title: 암기왕, Time: 1524 ms, Memory: 280076 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.java
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -1,36 +1,42 @@
-import java.util.*;
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int T = Integer.parseInt(br.readLine());
 
-        int testCase = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < testCase; i++) {
-            int N = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            int n = Integer.parseInt(br.readLine());
             StringTokenizer st = new StringTokenizer(br.readLine());
-
-            HashMap<Integer, Boolean> hm = new HashMap<>();
-            for (int j = 0; j < N; j++) {
-                hm.put(Integer.parseInt(st.nextToken()),true);
+            
+            Map<Integer, Boolean> hm = new HashMap<>();
+            for (int i = 0; i < n; i++) {
+                hm.put(Integer.parseInt(st.nextToken()), true);
             }
 
-            int M = Integer.parseInt(br.readLine());
+            int m = Integer.parseInt(br.readLine());
 
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                if (hm.containsKey(Integer.parseInt(st.nextToken()))){
+            for (int i = 0; i < m; i++) {
+                if (hm.containsKey(Integer.parseInt(st.nextToken()))) {
                     bw.write("1\n");
-                }
-                else {
+                } else {
                     bw.write("0\n");
                 }
             }
+
         }
         bw.flush();
         bw.close();
         br.close();
     }
 }
+
+
+
