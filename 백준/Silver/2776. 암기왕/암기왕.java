939ac3f1836ddb1a5f709a2aa861516e927843c7
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2022-08-01 22:43:11 +0900

    [Silver IV] Title: 암기왕, Time: 1532 ms, Memory: 280128 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.java
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -1,64 +1,36 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-
-    public static boolean flag = false;
-    public static int[] arr1;
-    public static int[] arr2;
-
-    public static void bin(int start, int end, int target) {
-        if (start > end) return;
-
-        int mid = (start + end) / 2;
-
-        if (arr1[mid] == target) {
-            flag = true;
-            return;
-        } else if (arr1[mid] > target) {
-            bin(start, mid - 1, target);
-        } else {
-            bin(mid + 1, end, target);
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        for (int t = 0; t < T; t++) {
-            int n = Integer.parseInt(br.readLine());
-            arr1 = new int[n];
+        int testCase = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < testCase; i++) {
+            int N = Integer.parseInt(br.readLine());
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < n; i++) {
-                arr1[i] = Integer.parseInt(st.nextToken());
+
+            HashMap<Integer, Boolean> hm = new HashMap<>();
+            for (int j = 0; j < N; j++) {
+                hm.put(Integer.parseInt(st.nextToken()),true);
             }
 
-            int m = Integer.parseInt(br.readLine());
-            arr2 = new int[m];
+            int M = Integer.parseInt(br.readLine());
+
             st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < m; i++) {
-                arr2[i] = Integer.parseInt(st.nextToken());
-            }
-
-            Arrays.sort(arr1);
-
-            StringBuilder sb = new StringBuilder();
-            for (int i : arr2) {
-                bin(0, arr1.length - 1, i);
-                if (!flag) {
-                    sb.append(0).append("\n");
-                } else {
-                    sb.append(1).append("\n");
-                    flag = false;
+            for (int j = 0; j < M; j++) {
+                if (hm.containsKey(Integer.parseInt(st.nextToken()))){
+                    bw.write("1\n");
+                }
+                else {
+                    bw.write("0\n");
                 }
             }
-            System.out.print(sb);
         }
-
+        bw.flush();
+        bw.close();
         br.close();
     }
 }
