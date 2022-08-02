8e66595a27fa50030153a054b252c5dfeb2faae3
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2022-08-03 00:56:49 +0900

    [Silver V] Title: 뒤집기, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -3,33 +3,9 @@ import sys
 input = sys.stdin.readline
 
 s = input().strip()
+cnt = 0
+for i in range(len(s) - 1):
+    if s[i] != s[i + 1]:
+        cnt += 1
 
-zero = 0
-one = 0
-
-i = 0
-
-while i < len(s):
-    if s[i] == "0":
-        for j in range(i, len(s)):
-            if s[j] != s[i]:
-                zero += 1
-                i = j
-                break
-            elif j == len(s) - 1:
-                zero += 1
-                i = j + 1
-                break
-
-    elif s[i] == "1":
-        for j in range(i, len(s)):
-            if s[j] != s[i]:
-                one += 1
-                i = j
-                break
-            elif j == len(s) - 1:
-                one += 1
-                i = j + 1
-                break
-
-print(min(zero, one))
+print((cnt + 1) // 2)
