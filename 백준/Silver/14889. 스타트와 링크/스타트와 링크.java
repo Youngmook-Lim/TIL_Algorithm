94ed699d28abad91e787dc935946dce53c02fdd3
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2022-08-08 23:12:00 +0900

    [Silver II] Title: 스타트와 링크, Time: 296 ms, Memory: 18712 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -10,66 +10,61 @@ public class Main {
 
     static int min = Integer.MAX_VALUE;
 
-    static List<Integer> list = new ArrayList<>();
-    static List<Integer> list2 = new ArrayList<>();
-
-    static Set<Integer> set = new HashSet<>();
-
-    static int getTotal(List<Integer> list) {
-        int total = 0;
-        for (int i = 0; i < list.size(); i++) {
-            for (int j = 0; j < list.size(); j++) {
-                if (i == j) continue;
-                total += graph[list.get(i) - 1][list.get(j) - 1];
-            }
-        }
-        return total;
-    }
-
-    static void dfs(int depth, int idx) {
-        if (depth == n / 2) {
-            Set<Integer> setTotal = new HashSet<>(set);
-            Set<Integer> listSet = new HashSet<>(list);
-            setTotal.removeAll(listSet);
-            list2.addAll(setTotal);
-            int a = getTotal(list);
-            int b = getTotal(list2);
-//            System.out.println(a + ", " + b);
-            int x = Math.abs(a - b);
-            if (x < min) {
-                min = x;
-            }
-            list2.clear();
-
-        }
-
-        for (int i = idx; i <= n; i++) {
-            if (i > idx && depth == 0) break;
-            list.add(i);
-            dfs(depth + 1, i + 1);
-            list.remove(list.size() - 1);
-
-        }
-    }
+    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         graph = new int[n][n];
+        visited = new boolean[n];
         StringTokenizer st;
         for (int i = 0; i < n; i++) {
-            set.add(i + 1);
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
                 graph[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        dfs(0, 1);
+        dfs(0, 0);
 
         System.out.println(min);
 
         br.close();
+
+    }
+
+    static void dfs(int depth, int idx) {
+        if (depth == n / 2) {
+            diff();
+        }
+
+        for (int i = idx; i < n; i++) {
+            if (i > idx && depth == 0) break;
+            visited[i] = true;
+            dfs(depth + 1, i + 1);
+            visited[i] = false;
+        }
+    }
+
+    static void diff() {
+        int start = 0;
+        int link = 0;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (visited[i] && visited[j]) {
+                    start += graph[i][j];
+                    start += graph[j][i];
+                } else if (!visited[i] && !visited[j]) {
+                    link += graph[i][j];
+                    link += graph[j][i];
+                }
+            }
+        }
+        int value = Math.abs(start - link);
+
+        if (value < min) {
+            min = value;
+        }
 
     }
 
