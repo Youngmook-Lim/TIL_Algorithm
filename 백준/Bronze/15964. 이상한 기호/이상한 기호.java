16537dabd98bf4417dd735db1381780b0872751f
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2022-08-09 09:40:18 +0900

    [Bronze V] Title: 이상한 기호, Time: 124 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Integer.parseInt(st.nextToken());
+        long b = Integer.parseInt(st.nextToken());
+
+        System.out.println(func(a, b));
+
+    }
+
+    static long func(long a, long b) {
+        return (a + b) * (a - b);
+    }
+}
+
+
+
