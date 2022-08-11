9e6007dd6328a0cdd65ae68a7e35670a04b68533
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2022-08-11 09:11:31 +0900

    [D2] Title: 중간 평균값 구하기, Time: 121 ms, Memory: 19,700 KB -BaekjoonHub

--- SWEA/D2/1984. 중간 평균값 구하기/중간 평균값 구하기.java
+++ SWEA/D2/1984. 중간 평균값 구하기/중간 평균값 구하기.java
@@ -1,12 +1,15 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
 
     static int[] arr;
+    static int[] sorted;
+    static int[] count;
+    static int max;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,19 +17,38 @@ public class Solution {
         for (int t = 1; t <= T; t++) {
             double ans = 0;
             arr = new int[10];
+            sorted = new int[10];
+            max = 0;
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int i = 0; i < 10; i++) {
                 arr[i] = Integer.parseInt(st.nextToken());
+                if (arr[i] > max) {
+                    max = arr[i];
+                }
             }
 
-            Arrays.sort(arr);
+            count = new int[max + 1];
+
+            for (int x : arr) {
+                count[x]++;
+            }
+
+            for (int i = 1; i < max + 1; i++) {
+                count[i] += count[i - 1];
+            }
+
+            for (int i = 9; i >= 0; i--) {
+                int idx = --count[arr[i]];
+                sorted[idx] = arr[i];
+            }
+
 
             for (int i = 1; i < 9; i++) {
-                ans += arr[i];
+                ans += sorted[i];
             }
 
             ans /= 8;
-            
+
             System.out.println("#" + t + " " + Math.round(ans));
         }
         br.close();
