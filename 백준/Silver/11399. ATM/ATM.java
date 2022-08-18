e145b1744bdf5cf01b2df9b5c0a4d683a0233596
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2022-08-19 01:49:13 +0900

    [Silver IV] Title: ATM, Time: 240 ms, Memory: 18628 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 1; i < n; i++) {
+            arr[i] += arr[i - 1];
+        }
+
+        bw.write(Arrays.stream(arr).sum() + "\n");
+
+
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+}
