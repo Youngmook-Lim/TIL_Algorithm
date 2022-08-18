873ccf1c3022cf16ac1f522df8b2dfd5eaf6afa3
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2022-08-18 11:17:50 +0900

    [Silver IV] Title: 카드2, Time: 84 ms, Memory: 11640 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,29 +1,24 @@
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Main {
 
     static int n;
-    static Queue<Integer> q;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         n = Integer.parseInt(br.readLine());
-        q = new LinkedList<>();
-        for (int i = 1; i <= n; i++) {
-            q.add(i);
-        }
 
-        while (q.size() != 1) {
-            q.poll();
-            int x = q.poll();
-            q.add(x);
-        }
+        int cnt = 1;
 
-        bw.write(q.peek() + "\n");
+        while (true) {
+            if (cnt >= n) {
+                bw.write(2 * n - cnt + "\n");
+                break;
+            }
+            cnt *= 2;
+        }
 
         bw.flush();
         bw.close();
