b18b3cc380c27a7b3447f214c2b0303a43f4f288
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2022-08-19 09:48:26 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 101 ms, Memory: 18,348 KB -BaekjoonHub

--- SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
@@ -1,44 +1,40 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Solution {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        int T = 10;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int T = Integer.parseInt(br.readLine());
         for (int t = 1; t <= T; t++) {
-            String memory = br.readLine();
-            String zeros = "";
-            int len = memory.length();
-            for (int i = 0; i < len; i++) {
-                zeros += "0";
-            }
-
+            char[] ans = br.readLine().toCharArray();
+            boolean flag = false;
+            char current = '0';
             int cnt = 0;
 
-            while (!memory.equals(zeros)) {
-                for (int i = 0; i < len; i++) {
-                    if (memory.charAt(i) != zeros.charAt(i)) {
-                        StringBuilder zerosString = new StringBuilder(zeros);
-                        for (int j = i; j < len; j++) {
-                            zerosString.setCharAt(j, memory.charAt(i));
-                        }
-                        zeros = zerosString.toString();
-                        break;
+            for (int i = 0; i < ans.length; i++) {
+                if (!flag) {
+                    if (ans[i] != current) {
+                        flag = true;
+                        current = (current == '1') ? '0' : '1';
+                        cnt++;
+                    }
+                } else {
+                    if (ans[i] != current) {
+                        current = (current == '1') ? '0' : '1';
+                        cnt++;
                     }
                 }
-                cnt++;
             }
 
-            System.out.println("#" + t + " " + cnt);
-
+            bw.write("#" + t + " " + cnt + "\n");
         }
 
-
+        bw.flush();
+        bw.close();
         br.close();
+
     }
 
 }
