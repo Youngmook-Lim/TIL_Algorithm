ef3fcb0c31abcab15fc9d822706eb4f74dced48f
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2022-08-27 01:01:26 +0900

    [Silver III] Title: N과 M (2), Time: 80 ms, Memory: 11668 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n, m;
+    static List<Integer> list;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        list = new ArrayList<>();
+        sb = new StringBuilder();
+
+        dfs(0, 1);
+
+        bw.write(sb.toString());
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static void dfs(int depth, int idx) {
+        if (depth == m) {
+            for (int x : list) {
+                sb.append(x).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = idx; i <= n; i++) {
+            list.add(i);
+            dfs(depth + 1, i + 1);
+            list.remove(list.size() - 1);
+        }
+    }
+
+}
