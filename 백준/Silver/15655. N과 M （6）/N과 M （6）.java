d177130d46f6108f951c153bfa3d641436dcd186
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2022-08-30 00:31:19 +0900

    [Silver III] Title: N과 M (6), Time: 88 ms, Memory: 11708 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static Stack<Integer> stack;
+    static int n, m;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        stack = new Stack<>();
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        dfs(0);
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static void dfs(int start) {
+
+        if (stack.size() == m) {
+            for (int x : stack) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            if (stack.contains(arr[i])) continue;
+            stack.push(arr[i]);
+            dfs(i + 1);
+            stack.pop();
+        }
+
+    }
+
+}
