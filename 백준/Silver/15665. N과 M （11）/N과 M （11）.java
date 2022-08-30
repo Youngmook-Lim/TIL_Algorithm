a14ed4a2888f7194c790c7df8d0a7edd67ec4c2d
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2022-08-31 00:18:59 +0900

    [Silver II] Title: N과 M (11), Time: 800 ms, Memory: 131356 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static Stack<Integer> stack;
+    static boolean[] visited;
+    static Set<List<Integer>> set;
+    static int n, m;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        stack = new Stack<>();
+        set = new LinkedHashSet<>();
+        arr = new int[n];
+        visited = new boolean[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        dfs();
+
+        StringBuilder sb = new StringBuilder();
+        for (List<Integer> x : set) {
+            for (int a : x) {
+                sb.append(a).append(" ");
+            }
+            sb.append("\n");
+
+        }
+
+        bw.write(sb.toString());
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static void dfs() {
+        if (stack.size() == m) {
+            set.add(new ArrayList<>(stack));
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            stack.push(arr[i]);
+            dfs();
+            stack.pop();
+        }
+    }
+}
