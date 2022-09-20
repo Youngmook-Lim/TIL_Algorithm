a05b74340d5a7b3fc29ca0018b8850b73f0d6519
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2022-09-20 11:19:09 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 80 ms, Memory: 11572 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,50 +1,57 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
 
-    static List<Integer> dwarfs = new ArrayList<>();
-    static int notDwarfsTotal;
+    static List<Integer> list;
+    static int[] dwarves;
+    static boolean flag;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        dwarves = new int[9];
+        list = new ArrayList<>();
+        flag = false;
 
         for (int i = 0; i < 9; i++) {
-            dwarfs.add(Integer.parseInt(br.readLine()));
+            dwarves[i] = Integer.parseInt(br.readLine());
         }
 
-        notDwarfsTotal = dwarfs.stream().mapToInt(x -> x).sum() - 100;
+        Arrays.sort(dwarves);
 
-        find();
+        dfs(0);
 
-        for (int x : dwarfs) {
-            if (x != 0) {
-                bw.write(x + "\n");
-            }
-        }
-
-        bw.flush();
-        bw.close();
         br.close();
 
     }
 
-    static void find() {
-        for (int i = 0; i < 8; i++) {
-            for (int j = i + 1; j < 9; j++) {
-                if (dwarfs.get(i) + dwarfs.get(j) == notDwarfsTotal) {
-                    dwarfs.set(i, 0);
-                    dwarfs.set(j, 0);
-                    Collections.sort(dwarfs);
-                    return;
-                }
+    static void dfs(int depth) {
+        if (flag) return;
+
+        if (depth == 7) {
+            int sum = 0;
+            for (int x : list) {
+                sum += dwarves[x];
             }
+            if (sum == 100) {
+                for (int x : list) {
+                    System.out.println(dwarves[x]);
+                }
+                flag = true;
+            }
+            return;
+
+        }
+
+        for (int i = depth; i < 9; i++) {
+            list.add(i);
+            dfs(depth + 1);
+            list.remove(list.size() - 1);
         }
     }
-
-
 }
-
