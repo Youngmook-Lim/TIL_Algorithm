a99e1591eb57394ecd9ee1f4ced506c1fa8baaff
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2022-09-23 14:06:25 +0900

    [Gold V] Title: 비요뜨의 징검다리 건너기, Time: 100 ms, Memory: 12244 KB -BaekjoonHub

--- 백준/Gold/18291. 비요뜨의 징검다리 건너기/비요뜨의 징검다리 건너기.java
+++ 백준/Gold/18291. 비요뜨의 징검다리 건너기/비요뜨의 징검다리 건너기.java
@@ -11,26 +11,34 @@ public class Main {
 
         int t = Integer.parseInt(br.readLine());
         int n;
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < t; i++) {
             n = Integer.parseInt(br.readLine());
-            long ans = power(2, n - 2);
-            System.out.println(ans);
+            if (n == 1 || n == 2) {
+                sb.append(1).append("\n");
+            } else {
+                long ans = power(2, n - 2);
+                sb.append(ans).append("\n");
+            }
         }
+
+        System.out.println(sb);
 
         br.close();
     }
 
     static long power(long a, int p) {
-        long ret = 1;
-        while (p > 0) {
-            if (p % 2 == 1) {
-                ret = (ret * a) % MOD;
-            }
-            a = (a * a) % MOD;
-            p /= 2;
+        if (p == 1) return a;
+
+        long ans = power(a, p / 2);
+
+        if (p % 2 == 0) {
+            return (ans * ans) % MOD;
+        } else {
+            return ((ans * ans) % MOD * a) % MOD;
         }
-        return ret;
+
     }
 
 
