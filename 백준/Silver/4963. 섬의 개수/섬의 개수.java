b44f53673c34a83b3dd43370864cf2bd459f4dca
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2022-09-23 09:18:39 +0900

    [Silver II] Title: 섬의 개수, Time: 116 ms, Memory: 13380 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -1,85 +1,63 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int w;
-    static int h;
-    static int[][] map;
+    static int w, h, ans;
+    static int[][] island;
     static int[] dx = {1, -1, 0, 0, 1, -1, -1, 1};
-    static int[] dy = {0, 0, 1, -1, 1, -1, 1, -1};
-
+    static int[] dy = {0, 0, 1, -1, 1, 1, -1, -1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+
         while (true) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             w = Integer.parseInt(st.nextToken());
             h = Integer.parseInt(st.nextToken());
-            int cnt = 0;
-
-            map = new int[h][w];
+            island = new int[h][w];
+            ans = 0;
 
             if (w == 0 && h == 0) break;
 
             for (int i = 0; i < h; i++) {
                 st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < w; j++) {
-                    map[i][j] = Integer.parseInt(st.nextToken());
+                    island[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
 
             for (int i = 0; i < h; i++) {
                 for (int j = 0; j < w; j++) {
-                    if (map[i][j] == 1) {
-                        bfs(i, j);
-                        cnt++;
+                    if (island[i][j] == 1) {
+                        dfs(i, j);
+                        ans++;
                     }
                 }
             }
 
-            System.out.println(cnt);
+            System.out.println(ans);
 
         }
+
         br.close();
-
     }
 
-    static void bfs(int y, int x) {
-        Queue<Pair> q = new LinkedList<>();
+    static void dfs(int y, int x) {
+        island[y][x] = 0;
 
-        q.add(new Pair(x, y));
-        map[y][x] = 0;
+        for (int k = 0; k < 8; k++) {
+            int nx = x + dx[k];
+            int ny = y + dy[k];
 
-        while (!q.isEmpty()) {
-            Pair p = q.poll();
+            if (ny < 0 || ny >= h || nx < 0 || nx >= w) continue;
 
-            for (int i = 0; i < 8; i++) {
-                int ny = p.y + dy[i];
-                int nx = p.x + dx[i];
-
-                if (nx >= 0 && nx < w && ny >= 0 && ny < h && map[ny][nx] == 1) {
-                    q.add(new Pair(nx, ny));
-                    map[ny][nx] = 0;
-                }
+            if (island[ny][nx] == 1) {
+                dfs(ny, nx);
             }
-
         }
     }
 
-}
-
-class Pair {
-    int x;
-    int y;
-
-    public Pair(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
 }
