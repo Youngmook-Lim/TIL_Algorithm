00a9ad26282d5e1458fbf399b9ee9ec583caa97e
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2022-09-27 13:13:07 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 452 ms, Memory: 85500 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -5,7 +5,7 @@ public class Main {
 
     static int k, w, h, ans;
     static int[][] graph;
-    static boolean[][][] visited;
+    static int[][] visited;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
     static int[] dxHorse = {1, 2, 2, 1, -1, -2, -2, -1};
@@ -20,16 +20,6 @@ public class Main {
             this.k = k;
             this.dist = dist;
         }
-
-        @Override
-        public String toString() {
-            return "P{" +
-                    "x=" + x +
-                    ", y=" + y +
-                    ", k=" + k +
-                    ", dist=" + dist +
-                    '}';
-        }
     }
 
     public static void main(String[] args) throws IOException {
@@ -42,7 +32,7 @@ public class Main {
         w = Integer.parseInt(st.nextToken());
         h = Integer.parseInt(st.nextToken());
         graph = new int[h][w];
-        visited = new boolean[h][w][k + 1];
+        visited = new int[h][w];
         ans = Integer.MAX_VALUE;
 
         for (int i = 0; i < h; i++) {
@@ -69,7 +59,7 @@ public class Main {
         Queue<P> q = new LinkedList<>();
 
         q.add(new P(0, 0, k, 0));
-        visited[0][0][k] = true;
+        visited[0][0] = (1 << k);
 
         while (!q.isEmpty()) {
             P p = q.poll();
@@ -84,9 +74,10 @@ public class Main {
                 int ny = p.y + dy[l];
 
                 if (nx < 0 || nx >= w || ny < 0 || ny >= h
-                        || graph[ny][nx] == 1 || visited[ny][nx][p.k]) continue;
+                        || graph[ny][nx] == 1
+                        || (visited[ny][nx] & (1 << p.k)) != 0) continue;
 
-                visited[ny][nx][p.k] = true;
+                visited[ny][nx] = (1 << p.k | visited[ny][nx]);
                 q.add(new P(ny, nx, p.k, p.dist + 1));
             }
 
@@ -96,16 +87,14 @@ public class Main {
                     int nyHorse = p.y + dyHorse[l];
 
                     if (nxHorse < 0 || nxHorse >= w || nyHorse < 0 || nyHorse >= h
-                            || graph[nyHorse][nxHorse] == 1 || visited[nyHorse][nxHorse][p.k - 1])
+                            || graph[nyHorse][nxHorse] == 1
+                            || (visited[nyHorse][nxHorse] & (1 << (p.k - 1))) != 0)
                         continue;
 
-                    visited[nyHorse][nxHorse][p.k - 1] = true;
+                    visited[nyHorse][nxHorse] = ((1 << (p.k - 1)) | visited[nyHorse][nxHorse]);
                     q.add(new P(nyHorse, nxHorse, p.k - 1, p.dist + 1));
-
                 }
             }
         }
     }
-
-
 }
