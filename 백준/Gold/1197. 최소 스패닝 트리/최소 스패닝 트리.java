15a952bcce385a5fa20ac0a1496839b5f56fbcb1
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2022-09-28 21:39:40 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 584 ms, Memory: 48620 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,75 +1,103 @@
 import java.io.*;
 import java.util.*;
-
+ 
 public class Main {
-    static final int INF = Integer.MAX_VALUE;
+ 
     static int v, e, pick;
     static long ans;
-    static List<Node>[] list;
-    static boolean[] visited;
-
-    static class Node implements Comparable<Node> {
-        int v, w;
-
-        public Node(int v, int w) {
-            this.v = v;
-            this.w = w;
+    static Edge[] list;
+    static int[] p;
+ 
+    static class Edge implements Comparable<Edge> {
+        int start, end, length;
+ 
+        public Edge(int start, int end, int length) {
+            this.start = start;
+            this.end = end;
+            this.length = length;
         }
-
+ 
         @Override
-        public int compareTo(Node o) {
-            return this.w - o.w;
+        public String toString() {
+            return "Edge{" +
+                    "start=" + start +
+                    ", end=" + end +
+                    ", length=" + length +
+                    '}';
+        }
+ 
+        @Override
+        public int compareTo(Edge o) {
+            return Integer.compare(this.length, o.length);
         }
     }
-
+ 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
-
+ 
         st = new StringTokenizer(br.readLine());
         v = Integer.parseInt(st.nextToken());
         e = Integer.parseInt(st.nextToken());
         ans = 0;
         pick = 0;
-        visited = new boolean[v + 1];
-        list = new ArrayList[v + 1];
-        for (int i = 0; i < v + 1; i++) {
-            list[i] = new ArrayList<>();
-        }
+        p = new int[v + 1];
+        list = new Edge[e];
         for (int i = 0; i < e; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
 
-            list[a].add(new Node(b, c));
-            list[b].add(new Node(a, c));
+            list[i] = new Edge(a, b, c);
         }
 
-        prim(1);
+        Arrays.sort(list);
+
+        kruskal();
 
         bw.write(ans + "\n");
-
-
+        
         bw.flush();
         bw.close();
         br.close();
     }
-
-    static void prim(int start) {
-        Queue<Node> pq = new PriorityQueue<>();
-        visited[start] = true;
-        pq.addAll(list[start]);
-        while (pick < v - 1) {
-            Node curr = pq.poll();
-
-            if (visited[curr.v]) continue;
-
-            visited[curr.v] = true;
-            ans += curr.w;
-            pq.addAll(list[curr.v]);
-            pick++;
+ 
+    static void kruskal() {
+        for (int i = 0; i < v + 1; i++) {
+            p[i] = i;
         }
+ 
+        for (int i = 0; i < e; i++) {
+            Edge edge = list[i];
+            int rootStart = findSet(edge.start);
+            int rootEnd = findSet(edge.end);
+ 
+            if (rootStart != rootEnd) {
+                union(rootStart, rootEnd);
+                ans += edge.length;
+                pick++;
+            }
+ 
+            if (pick == v - 1) {
+                break;
+            }
+ 
+        }
+ 
     }
+ 
+    static void union(int x, int y) {
+        p[y] = x;
+    }
+ 
+    static int findSet(int x) {
+        if (p[x] != x) {
+            p[x] = findSet(p[x]);
+        }
+        return p[x];
+    }
+ 
+ 
 }
