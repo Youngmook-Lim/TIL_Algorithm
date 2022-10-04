e46125c6e56b2712e97b0558405148adebf8dd95
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2022-10-05 01:29:12 +0900

    [Bronze II] Title: 커트라인, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+  int n, k;
+
+  cin >> n >> k;
+
+  int *arr = new int[n];
+
+  for (int i = 0; i < n; i++)
+  {
+    cin >> arr[i];
+  }
+
+  sort(arr, arr + n);
+
+  cout << arr[n - k];
+
+  return 0;
+}
