8d049e8fab39918be1272019186de7f4919215f9
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2022-10-04 23:17:07 +0900

    [Gold I] Title: Brainf**k 인터프리터, Time: 4808 ms, Memory: 22464 KB -BaekjoonHub

--- 백준/Gold/3954. Brainf＊＊k 인터프리터/Brainf＊＊k 인터프리터.java
+++ 백준/Gold/3954. Brainf＊＊k 인터프리터/Brainf＊＊k 인터프리터.java
@@ -1,114 +1,127 @@
-import java.io.*;
+/*
+* 내 코드 아님; 속도 확인용
+*/
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int Sm;
+    static int Sc;
+    static int Si;
+    static String programCode;
+    static String inputCode;
+    static int pointerPos;
+    static int inputPos;
+    static int[] arr;
+    static int[] bracket;
+    static int pid;
 
-    static final int LIMIT = 50000000;
-    static Stack<Integer> stack;
-    static int[] memory, brackets;
-    static int sm, sc, si, idx, pointer, inputIdx, cnt;
-
-    static String code, input;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
         int T = Integer.parseInt(br.readLine());
-
-        for (int t = 0; t < T; t++) {
+        StringBuilder ans = new StringBuilder();
+        while (T-- > 0) {
             st = new StringTokenizer(br.readLine());
-            sm = Integer.parseInt(st.nextToken());
-            sc = Integer.parseInt(st.nextToken());
-            si = Integer.parseInt(st.nextToken());
-            code = br.readLine();
-            input = br.readLine();
-            stack = new Stack<>();
-            memory = new int[sm];
-            brackets = new int[sc];
+            Sm = Integer.parseInt(st.nextToken()); // 배열의 크기
+            Sc = Integer.parseInt(st.nextToken()); // 명령어의 길이
+            Si = Integer.parseInt(st.nextToken()); // input의 길이
 
-            for (int i = 0; i < sc; i++) {
-                char cur = code.charAt(i);
-                if (cur == '[') {
+            programCode = br.readLine();
+            inputCode = br.readLine();
+
+            arr = new int[Sm];
+            pointerPos = 0; // 포인터가 가리키는 위치.
+            inputPos = 0; // inputCode의 현재 입력받아야하는 위치.
+
+            Stack<Integer> stack = new Stack<>();
+            bracket = new int[Sc]; // 서로 연결되어있는 괄호의 위치.
+
+            for (int i = 0; i < Sc; i++) {
+                char c = programCode.charAt(i);
+
+                if (c == '[') {
                     stack.push(i);
-                } else if (cur == ']') {
-                    int startBracket = stack.pop();
-                    brackets[startBracket] = i;
-                    brackets[i] = startBracket;
+                } else if (c == ']') { // 닫는 괄호와 여는 괄호를 서로 연결시켜 줌.
+                    int temp = stack.peek();
+                    bracket[i] = temp; // 여는 괄호에는 닫는 괄호의 위치를
+                    bracket[temp] = i; // 닫는 괄호에는 여는 괄호의 위치를 저장함.
+                    stack.pop();
                 }
             }
 
-            idx = 0;
-            pointer = 0;
-            inputIdx = 0;
-            cnt = 0;
-
-            while (idx < sc && cnt++ <= LIMIT) {
-                runProgram();
+            pid = 0; // 특정 명령어를 읽은 위치.
+            int cnt = 0; // 반복 횟수.
+            while (cnt <= 50000000 && pid < Sc) {
+                ++cnt;
+                doStep();
             }
 
-            if (idx == sc) {
-                sb.append("Terminates\n");
-            } else {
-                int maxLoopIdx, minLoopIdx;
-                maxLoopIdx = minLoopIdx = idx;
+            if (pid == Sc) { // 해당 명령어를 모두 읽어들임.
+                ans.append("Terminates\n");
+            } else { // 도중에 무한루프가 발생.
+                // 일단 50000000번만큼 이동한 위치를 기억해 둔 상태에서
+                // 50000000번만큼 다시 명령어를 읽어 들임.
+                // 그때, 최대 위치가 닫는 괄호의 위치이고
+                // 최소 위치는 여는 괄호의 위치 - 1임.
+
+                int maxpid = pid; // 닫는 괄호의 위치
+                int minpid = pid; // 여는 괄호의 위치
+
                 while (cnt-- > 0) {
-                    runProgram();
-                    minLoopIdx = Math.min(minLoopIdx, idx);
-                    maxLoopIdx = Math.max(maxLoopIdx, idx);
+                    doStep();
+                    maxpid = Math.max(maxpid, pid);
+                    minpid = Math.min(minpid, pid);
                 }
-
-                sb.append("Loops " + (minLoopIdx - 1) + " " + maxLoopIdx + "\n");
+                ans.append("Loops " + (minpid - 1) + " " + maxpid + "\n");
             }
         }
 
-        bw.write(sb.toString());
+        bw.write(ans.toString());
         bw.flush();
         bw.close();
         br.close();
     }
 
-    static void runProgram() {
-        char cmd = code.charAt(idx);
+    public static void doStep() {
+        char c = programCode.charAt(pid);
 
-        switch (cmd) {
-            case '+':
-                memory[pointer] = (memory[pointer] == 255) ? 0 : memory[pointer] + 1;
-                break;
+        switch (c) {
             case '-':
-                memory[pointer] = (memory[pointer] == 0) ? 255 : memory[pointer] - 1;
+                arr[pointerPos] = (arr[pointerPos] - 1) < 0 ? 255 : (arr[pointerPos] - 1);
+                break;
+            case '+':
+                arr[pointerPos] = (arr[pointerPos] + 1) > 255 ? 0 : (arr[pointerPos] + 1);
                 break;
             case '<':
-                pointer = (pointer == 0) ? sm - 1 : pointer - 1;
+                pointerPos = (pointerPos - 1) == -1 ? Sm - 1 : (pointerPos - 1);
                 break;
             case '>':
-                pointer = (pointer == sm - 1) ? 0 : pointer + 1;
+                pointerPos = (pointerPos + 1) == Sm ? 0 : (pointerPos + 1);
+                break;
+            case '[':
+                if (arr[pointerPos] == 0) {
+                    pid = bracket[pid]; // 현재 위치를 닫는 괄호의 위치로 점프함.
+                }
+                break;
+            case ']':
+                if (arr[pointerPos] != 0) {
+                    pid = bracket[pid]; // 현재 위치를 여는 괄호의 위치로 점프함.
+                }
                 break;
             case '.':
                 break;
             case ',':
-                if (inputIdx == si) {
-                    memory[pointer] = 255;
-                } else {
-                    memory[pointer] = input.charAt(inputIdx++);
-                }
-                break;
-            case '[':
-                if (memory[pointer] == 0) {
-                    idx = brackets[idx];
-                }
-                break;
-            case ']':
-                if (memory[pointer] != 0) {
-                    idx = brackets[idx];
-                }
-                break;
+                arr[pointerPos] = (inputPos == Si) ? 255 : inputCode.charAt(inputPos++);
         }
-        idx++;
+        pid++;
     }
-
 }
-
