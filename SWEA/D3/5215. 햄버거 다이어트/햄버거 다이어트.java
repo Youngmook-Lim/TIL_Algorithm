b8a314667681bc960918cdddf36b8a649ff41f90
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2022-10-06 13:41:59 +0900

    [D3] Title: 햄버거 다이어트, Time: 792 ms, Memory: 21,248 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,57 +1,60 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Solution {
 
-	static int n, l, maxPoints;
-	static boolean[] check;
-	static int[][] ingredients;
+    static int n, l;
+    static int[][] dp;
+    static Ingredient[] hamburgers;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
+    static class Ingredient {
+        int score, cal;
 
-		int T = Integer.parseInt(br.readLine());
-		for (int t = 1; t <= T; t++) {
-			st = new StringTokenizer(br.readLine());
-			n = Integer.parseInt(st.nextToken());
-			l = Integer.parseInt(st.nextToken());
-			check = new boolean[n];
-			ingredients = new int[n][2];
-			maxPoints = 0;
+        public Ingredient(int score, int cal) {
+            this.score = score;
+            this.cal = cal;
+        }
+    }
 
-			for (int i = 0; i < n; i++) {
-				st = new StringTokenizer(br.readLine());
-				ingredients[i][0] = Integer.parseInt(st.nextToken());
-				ingredients[i][1] = Integer.parseInt(st.nextToken());
-			}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-			solve(0, 0, 0);
+        int T = Integer.parseInt(br.readLine());
 
-			bw.write("#" + t + " " + maxPoints + "\n");
-		}
+        for (int t = 1; t <= T; t++) {
+            st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            l = Integer.parseInt(st.nextToken());
+            dp = new int[n + 1][l + 1];
+            hamburgers = new Ingredient[n + 1];
+            for (int i = 1; i < n + 1; i++) {
+                st = new StringTokenizer(br.readLine());
+                int score = Integer.parseInt(st.nextToken());
+                int cal = Integer.parseInt(st.nextToken());
+                hamburgers[i] = new Ingredient(score, cal);
+            }
 
-		bw.flush();
-		bw.close();
-		br.close();
-	}
+            for (int i = 1; i < n + 1; i++) {
+                for (int j = 1; j < l + 1; j++) {
+                    Ingredient cur = hamburgers[i];
+                    if (cur.cal <= j) {
+                        dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - cur.cal] + cur.score);
+                    } else {
+                        dp[i][j] = dp[i - 1][j];
+                    }
+                }
+            }
 
-	static void solve(int idx, int tSum, int kSum) {
-		if (kSum > l)
-			return;
-		if (idx == n) {
-			if (kSum <= l) {
-				maxPoints = Math.max(maxPoints, tSum);
-			}
-			return;
-		}
+            sb.append("#" + t + " " + dp[n][l]).append('\n');
 
-		solve(idx + 1, tSum + ingredients[idx][0], kSum + ingredients[idx][1]);
-		solve(idx + 1, tSum, kSum);
-	}
+        }
+        System.out.println(sb);
+        br.close();
+    }
+
+
 }
