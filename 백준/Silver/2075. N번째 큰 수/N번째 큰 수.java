8d5f439b0c4d1e655069355aae417b6092f00255
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2022-10-29 01:47:40 +0900

    [Silver II] Title: N번째 큰 수, Time: 796 ms, Memory: 296392 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -19,16 +19,23 @@ public class Main {
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                pq.add(-Integer.parseInt(st.nextToken()));
+            if (i == 0) {
+                for (int j = 0; j < n; j++) {
+                    pq.add(Integer.parseInt(st.nextToken()));
+                }
+            } else {
+                for (int j = 0; j < n; j++) {
+                    int tmp = Integer.parseInt(st.nextToken());
+                    if (tmp > pq.peek()) {
+                        pq.poll();
+                        pq.add(tmp);
+                    }
+                }
             }
-        }
 
-        for (int i = 0; i < n - 1; i++) {
-            pq.poll();
         }
-
-        System.out.println(-pq.poll());
+        
+        System.out.println(pq.poll());
 
         br.close();
     }
