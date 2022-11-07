503f57dad58ac2f460a9cd7c920449b8ed08be76
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2022-11-08 00:57:45 +0900

    [Silver I] Title: 1로 만들기 2, Time: 80 ms, Memory: 15548 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
@@ -8,7 +8,7 @@ public class Main {
 
     static int n;
     static int[] arr;
-    static String ans;
+    static Deque<Integer> ans;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,10 +18,10 @@ public class Main {
         int cnt = 0;
         n = Integer.parseInt(br.readLine());
 
-        arr[1] = -1;
+        arr[n] = -1;
 
         Queue<Integer> q = new ArrayDeque<>();
-        q.add(1);
+        q.add(n);
 
         loop:
         while (!q.isEmpty()) {
@@ -30,40 +30,44 @@ public class Main {
             for (int i = 0; i < size; i++) {
                 int cur = q.poll();
 
-                if (cur == n) {
+                if (cur == 1) {
                     ans = getPath();
                     break loop;
                 }
 
-                if (cur * 3 <= n && arr[cur * 3] == 0) {
-                    arr[cur * 3] = cur;
-                    q.add(cur * 3);
+                if (cur % 3 == 0 && cur / 3 >= 1 && arr[cur / 3] == 0) {
+                    arr[cur / 3] = cur;
+                    q.add(cur / 3);
                 }
-                if (cur * 2 <= n && arr[cur * 2] == 0) {
-                    arr[cur * 2] = cur;
-                    q.add(cur * 2);
+                if (cur % 2 == 0 && cur / 2 >= 1 && arr[cur / 2] == 0) {
+                    arr[cur / 2] = cur;
+                    q.add(cur / 2);
                 }
-                if (cur + 1 <= n && arr[cur + 1] == 0) {
-                    arr[cur + 1] = cur;
-                    q.add(cur + 1);
+                if (cur - 1 >= 1 && arr[cur - 1] == 0) {
+                    arr[cur - 1] = cur;
+                    q.add(cur - 1);
                 }
             }
         }
 
-        System.out.println(cnt - 1);
-        System.out.println(ans);
+        StringBuilder sb = new StringBuilder();
+        sb.append(cnt - 1).append('\n');
+        for (int x : ans) {
+            sb.append(x).append(' ');
+        }
 
-
+        System.out.println(sb);
         br.close();
     }
 
-    static String getPath() {
-        StringBuilder sb = new StringBuilder();
-        while (n != -1) {
-            sb.append(n).append(' ');
-            n = arr[n];
+    static Deque<Integer> getPath() {
+        Deque<Integer> result = new ArrayDeque<>();
+        int start = 1;
+        while (start != -1) {
+            result.addFirst(start);
+            start = arr[start];
         }
-        return sb.toString();
+        return result;
     }
 
 
