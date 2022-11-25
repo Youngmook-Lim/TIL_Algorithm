ff760f9c800d0abecda2dea0a2a16e85bb78bc07
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2022-11-26 01:30:12 +0900

    [Bronze II] Title: 대표값2, Time: 80 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+class Main {
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[5];
+        int total = 0;
+
+        for (int i = 0; i < 5; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            total += arr[i];
+        }
+        Arrays.sort(arr);
+
+        System.out.println(total / 5);
+        System.out.println(arr[2]);
+
+        br.close();
+
+    }
+
+
+}
