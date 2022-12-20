d573d99447a76fd45f4d4605be3bdba85d48cf5a
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2022-12-21 00:36:04 +0900

    [Gold I] Title: 부분수열의 합 2, Time: 632 ms, Memory: 125164 KB -BaekjoonHub

--- 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.java
+++ 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.java
@@ -7,7 +7,6 @@ public class Main {
     static int n, s;
     static long ans;
     static int[] arr;
-    static List<Integer> listLeft, listRight;
     static Map<Integer, Integer> hashMap;
 
     public static void main(String[] args) throws Exception {
@@ -23,9 +22,8 @@ public class Main {
 
         hashMap = new HashMap<>();
 
-        createPowerSet(0, n / 2, 1);
-        createPowerSet(n / 2 + 1, n - 1, 2);
-
+        createPowerSet(0, n / 2, 0, 1);
+        createPowerSet(n / 2 + 1, n - 1, 0, 2);
 
         if (s == 0) {
             ans--;
@@ -36,21 +34,18 @@ public class Main {
         br.close();
     }
 
-    static void createPowerSet(int start, int end, int type) {
-        int num = end - start + 1;
-        for (int i = 0; i < (1 << num); i++) {
-            int sum = 0;
-            for (int j = 0; j < num; j++) {
-                if ((i & (1 << j)) != 0) {
-                    sum += arr[start + j];
-                }
-            }
+    static void createPowerSet(int start, int end, int sum, int type) {
+        if (start > end) {
             if (type == 1) {
                 hashMap.put(sum, hashMap.getOrDefault(sum, 0) + 1);
             } else {
                 ans += hashMap.getOrDefault(s - sum, 0);
             }
+            return;
         }
+        createPowerSet(start + 1, end, sum, type);
+        createPowerSet(start + 1, end, sum + arr[start], type);
+
     }
 
 
