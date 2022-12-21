b6ea7a4a0281242d886f6b94c994158e3e6a0c2e
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2022-12-22 00:09:24 +0900

    [Gold IV] Title: 문제 추천 시스템 Version 1, Time: 444 ms, Memory: 46208 KB -BaekjoonHub

--- 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.java
+++ 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.java
@@ -6,7 +6,7 @@ public class Main {
 
     static int n, m;
     static TreeSet<P> ts;
-    static Map<Integer, Integer> pos;
+    static int[] pos;
 
     static class P implements Comparable<P> {
         int p, l;
@@ -32,13 +32,13 @@ public class Main {
 
         n = Integer.parseInt(br.readLine());
         ts = new TreeSet<>();
-        pos = new HashMap<>();
+        pos = new int[100_001];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             int p = Integer.parseInt(st.nextToken());
             int l = Integer.parseInt(st.nextToken());
             ts.add(new P(p, l));
-            pos.put(p, l);
+            pos[p] = l;
         }
 
         m = Integer.parseInt(br.readLine());
@@ -56,12 +56,10 @@ public class Main {
                 int p = Integer.parseInt(st.nextToken());
                 int l = Integer.parseInt(st.nextToken());
                 ts.add(new P(p, l));
-                pos.put(p, l);
+                pos[p] = l;
             } else {
                 int p = Integer.parseInt(st.nextToken());
-                P rm = new P(p, pos.get(p));
-                ts.remove(rm);
-                pos.remove(p);
+                ts.remove(new P(p, pos[p]));
             }
         }
 
