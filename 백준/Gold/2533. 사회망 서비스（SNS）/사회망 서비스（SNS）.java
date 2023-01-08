ccaecb940172fc0e07149b5224beb9963355e135
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2023-01-09 01:25:20 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 1436 ms, Memory: 367404 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
@@ -5,31 +5,43 @@ import java.util.*;
 public class Main {
 
     static int n;
-    static List<Integer>[] adj;
+    static Node[] tree;
     static int[][] dp;
     static boolean[] visited;
+
+    static class Node {
+        int n;
+        Node next;
+
+        public Node(int n, Node next) {
+            this.n = n;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "n=" + n +
+                    ", next=" + next +
+                    '}';
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         n = Integer.parseInt(br.readLine());
-        adj = new List[n + 1];
-        for (int i = 1; i < n + 1; i++) {
-            adj[i] = new ArrayList<>();
-        }
+        tree = new Node[n + 1];
         dp = new int[n + 1][2];
-        for (int i = 0; i < n + 1; i++) {
-            Arrays.fill(dp[i], -1);
-        }
         visited = new boolean[n + 1];
 
         for (int i = 0; i < n - 1; i++) {
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-            adj[u].add(v);
-            adj[v].add(u);
+            tree[u] = new Node(v, tree[u]);
+            tree[v] = new Node(u, tree[v]);
         }
 
         getEarlyAdaptors(1);
@@ -40,16 +52,16 @@ public class Main {
     }
 
     static void getEarlyAdaptors(int cur) {
-        
+
         visited[cur] = true;
         dp[cur][0] = 0;
         dp[cur][1] = 1;
 
-        for (int x : adj[cur]) {
-            if (visited[x]) continue;
-            getEarlyAdaptors(x);
-            dp[cur][0] += dp[x][1];
-            dp[cur][1] += Math.min(dp[x][0], dp[x][1]);
+        for (Node next = tree[cur]; next != null; next = next.next) {
+            if (visited[next.n]) continue;
+            getEarlyAdaptors(next.n);
+            dp[cur][0] += dp[next.n][1];
+            dp[cur][1] += Math.min(dp[next.n][0], dp[next.n][1]);
         }
 
     }
