416b898fffc97972230c9ef71dabaf228a535852
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2023-01-13 01:25:06 +0900

    [Silver V] Title: 행렬 곱셈, Time: 288 ms, Memory: 19048 KB -BaekjoonHub

---
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, m, k;
+    static int[][] a, b, ans;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        a = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                a[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        b = new int[m][k];
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < k; j++) {
+                b[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        ans = new int[n][k];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < k; j++) {
+                int sum = 0;
+                for (int l = 0; l < m; l++) {
+                    sum += a[i][l] * b[l][j];
+                }
+                ans[i][j] = sum;
+            }
+        }
+
+        for (int[] arr : ans) {
+            for (int x : arr) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+
+
+        br.close();
+    }
+
+
+}
