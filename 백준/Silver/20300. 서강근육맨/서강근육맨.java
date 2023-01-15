6296de1a42a50569b0c948e77daccfd384667b99
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2023-01-16 00:30:14 +0900

    [Silver III] Title: 서강근육맨, Time: 168 ms, Memory: 16872 KB -BaekjoonHub

---
+++ 백준/Silver/20300. 서강근육맨/서강근육맨.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n;
+    static long ans;
+    static long[] arr;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+        arr = new long[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Long.parseLong(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n / 2; i++) {
+            long sum;
+            if (n % 2 == 0) {
+                sum = arr[i] + arr[n - i - 1];
+            } else {
+                sum = arr[i] + arr[n - i - 2];
+            }
+            ans = Math.max(ans, sum);
+        }
+
+        if (n % 2 != 0) {
+            ans = Math.max(ans, arr[n - 1]);
+        }
+
+        System.out.println(ans);
+
+        br.close();
+    }
+
+
+}
