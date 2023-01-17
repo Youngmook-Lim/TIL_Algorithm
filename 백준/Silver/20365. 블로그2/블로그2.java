e7c7526ce6eafa92c1f4f3e824781ffc12ed9b94
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2023-01-18 01:25:32 +0900

    [Silver III] Title: 블로그2, Time: 156 ms, Memory: 17552 KB -BaekjoonHub

---
+++ 백준/Silver/20365. 블로그2/블로그2.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        String input = br.readLine();
+        int cntB = 0;
+        int cntR = 0;
+        char cur = input.charAt(0);
+        if (cur == 'B') {
+            cntB++;
+        } else {
+            cntR++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (cur != input.charAt(i)) {
+                if (cur == 'B') {
+                    cntR++;
+                } else {
+                    cntB++;
+                }
+                cur = input.charAt(i);
+            }
+        }
+
+        System.out.println(Math.min(cntB + cntR, Math.min(1 + cntB, 1 + cntR)));
+
+        br.close();
+    }
+
+
+}
+
+
+
