627dfdcc1bbe7e91c6546ef7d8dfb11824f50ca7
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2023-02-28 00:10:06 +0900

    [Bronze II] Title: 공 바꾸기, Time: 76 ms, Memory: 11728 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] basket = new int[n];
+        for (int i = 0; i < n; i++) {
+            basket[i] = i + 1;
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int I = Integer.parseInt(st.nextToken()) - 1;
+            int J = Integer.parseInt(st.nextToken()) - 1;
+
+            swap(basket, I, J);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int x : basket) {
+            sb.append(x).append(' ');
+        }
+        System.out.println(sb);
+
+        br.close();
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+
+}
+
+
