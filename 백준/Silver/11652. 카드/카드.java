ab433bcd26509fd73464d3fbafa8eb34bb0e37cd
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2023-03-04 00:35:41 +0900

    [Silver IV] Title: 카드, Time: 308 ms, Memory: 38376 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.java
+++ 백준/Silver/11652. 카드/카드.java
@@ -3,60 +3,37 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
-import java.util.TreeSet;
 
 
 public class Main {
-
-    static class Num implements Comparable<Num> {
-        long number;
-        int cnt;
-
-        public Num(long number, int cnt) {
-            this.number = number;
-            this.cnt = cnt;
-        }
-
-        @Override
-        public int compareTo(Num o) {
-            if (this.cnt != o.cnt) {
-                return Integer.compare(o.cnt, this.cnt);
-            }
-            return Long.compare(this.number, o.number);
-        }
-
-        @Override
-        public String toString() {
-            return "Num{" +
-                    "number=" + number +
-                    ", cnt=" + cnt +
-                    '}';
-        }
-    }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
+
+        int maxCnt = 0;
+        long selected = Long.MAX_VALUE;
+
         int n = Integer.parseInt(br.readLine());
-        TreeSet<Num> ts = new TreeSet<>();
-        Map<Long, Num> map = new HashMap<>();
+
+        Map<Long, Integer> map = new HashMap<>();
 
         for (int i = 0; i < n; i++) {
             long number = Long.parseLong(br.readLine());
-            if (map.containsKey(number)) {
-                Num num = map.get(number);
-                ts.remove(num);
-                num.cnt++;
-                ts.add(num);
-            } else {
-                Num num = new Num(number, 1);
-                map.put(number, num);
-                ts.add(num);
+            map.put(number, map.getOrDefault(number, 0) + 1);
+            if (map.get(number) >= maxCnt) {
+                if (map.get(number) == maxCnt) {
+                    selected = Math.min(selected, number);
+                } else {
+                    maxCnt = map.get(number);
+                    selected = number;
+                }
             }
         }
 
-        System.out.println(ts.first().number);
+        System.out.println(selected);
+
 
         br.close();
     }
