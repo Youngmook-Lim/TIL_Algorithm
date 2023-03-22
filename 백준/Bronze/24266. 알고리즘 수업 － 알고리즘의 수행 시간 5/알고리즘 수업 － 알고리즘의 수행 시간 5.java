4c33d08505715b460f253f5918733bfbab96adc3
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2023-03-23 03:43:41 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 76 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        long n = Long.parseLong(br.readLine());
+        System.out.println(n * n * n);
+        System.out.println(3);
+
+        br.close();
+    }
+
+
+}
+
