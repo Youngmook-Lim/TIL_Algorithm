d03424488410db644deea0ede2ed41cfe607f261
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2023-04-12 17:46:40 +0900

    [Silver III] Title: 수 이어 쓰기, Time: 104 ms, Memory: 13732 KB -BaekjoonHub

--- 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.java
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.java
@@ -1,52 +1,38 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 
 public class Main {
 
-    static List<Character> str;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         String input = br.readLine();
-        str = new ArrayList<>();
 
         int n = 1;
+        int idx = 0;
 
         while (true) {
-            createString(n);
-            int idx = 0;
+            String nString = Integer.toString(n);
 
-            for (int i = 0; i < str.size(); i++) {
-                char cur = str.get(i);
+            for (int i = 0; i < nString.length(); i++) {
+                char cur = nString.charAt(i);
                 if (cur == input.charAt(idx)) {
                     idx++;
                 }
-                if (idx == input.length()) break;
-            }
-
-            if (idx == input.length()) {
-                break;
+                if (idx >= input.length()) {
+                    System.out.println(n);
+                    return;
+                }
             }
 
             n++;
         }
 
-        System.out.println(n);
-
-        br.close();
-    }
-
-    static void createString(int n) {
-        String nString = Integer.toString(n);
-        for (int i = 0; i < nString.length(); i++) {
-            str.add(nString.charAt(i));
-        }
+//        br.close();
     }
 
 }
