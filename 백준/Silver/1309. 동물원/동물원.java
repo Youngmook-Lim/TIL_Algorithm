13a8c79857c0e5b420ce3b1626208eab1266451c
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2023-05-20 01:27:01 +0900

    [Silver I] Title: 동물원, Time: 96 ms, Memory: 15012 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Main {
+
+    static int n;
+    static final int MOD = 9901;
+    static int[][] dp;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+        dp = new int[n][3];
+        dp[0][0] = 1;
+        dp[0][1] = 1;
+        dp[0][2] = 1;
+
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % MOD;
+            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % MOD;
+            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % MOD;
+        }
+
+        System.out.println((dp[n - 1][0] + dp[n - 1][1] + dp[n - 1][2]) % MOD);
+
+        br.close();
+    }
+
+
+}
