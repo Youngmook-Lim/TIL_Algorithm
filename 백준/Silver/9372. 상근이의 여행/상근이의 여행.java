8ab32053aa0d284152e1a424c49f0d2bb42f55d3
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2023-06-05 17:15:00 +0900

    [Silver IV] Title: 상근이의 여행, Time: 176 ms, Memory: 22844 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -1,11 +1,10 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.List;
 import java.util.StringTokenizer;
 
 
 public class Main {
-
+    
     static int n, m, T;
 
     public static void main(String[] args) throws Exception {
@@ -23,9 +22,7 @@ public class Main {
 
 
             for (int i = 0; i < m; i++) {
-                st = new StringTokenizer(br.readLine());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
+                br.readLine();
             }
 
             sb.append(n - 1).append('\n');
