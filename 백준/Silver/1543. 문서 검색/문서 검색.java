37f6de2e612c3f482e089be5f3177293d4abd985
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2023-06-10 19:04:52 +0900

    [Silver IV] Title: 문서 검색, Time: 120 ms, Memory: 12212 KB -BaekjoonHub

---
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        String str = br.readLine();
+        String word = br.readLine();
+        int strLen = str.length();
+        int wordLen = word.length();
+        int max = 0;
+
+        for (int i = 0; i <= strLen - wordLen; i++) {
+            int cnt = 0;
+            for (int j = i; j <= strLen - wordLen; j++) {
+                boolean flag = true;
+                for (int k = 0; k < wordLen; k++) {
+                    if (str.charAt(j) == word.charAt(k)) {
+                        j++;
+                    } else {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    cnt++;
+                    j--;
+                }
+            }
+            max = Math.max(max, cnt);
+        }
+
+        System.out.println(max);
+
+        br.close();
+    }
+
+
+}
