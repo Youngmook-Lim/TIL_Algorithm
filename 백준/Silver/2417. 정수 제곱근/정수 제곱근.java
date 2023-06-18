afafa8d1358c12e059a07b28170e79f51d2f8b0e
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2023-06-19 01:16:19 +0900

    [Silver IV] Title: 정수 제곱근, Time: 76 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Silver/2417. 정수 제곱근/정수 제곱근.java
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.java
@@ -5,6 +5,7 @@ import java.util.StringTokenizer;
 class Main {
 
     static long n, ans;
+    static final double MAX_SQRT = Math.sqrt(Long.MAX_VALUE);
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,10 +23,16 @@ class Main {
 
     static void bs(long start, long end) {
         if (start > end) return;
+//        System.out.println(start + " " + end);
 
         long mid = (start + end) / 2;
 
-        long res = (long) Math.pow(mid, 2);
+        double res;
+        if (mid >= MAX_SQRT) {
+            res = n;
+        } else {
+            res = Math.pow(mid, 2);
+        }
 
         if (res >= n) {
             ans = mid;
