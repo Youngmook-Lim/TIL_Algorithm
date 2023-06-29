1d81d6ac322c0d436d88717e490b22b3b747e2e7
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2023-06-30 02:04:20 +0900

    [Silver III] Title: 통계학, Time: 908 ms, Memory: 120416 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -0,0 +1,48 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+nums = []
+for _ in range(n):
+    nums.append(int(input()))
+
+# 1
+print(round(sum(nums) / n))
+
+# 2
+nums.sort()
+print(nums[int((n-1)/2)])
+
+# 3
+counts = dict()
+for i in range(1, n+1):
+    counts[i] = []
+
+maxCount = 1
+count = 1
+
+for j in range(1, n):
+    if nums[j] == nums[j-1]:
+        count += 1
+    else:
+        counts[count].append(nums[j-1])
+        if maxCount < count:
+            maxCount = count
+        count = 1
+    if j == n-1:
+        counts[count].append(nums[j])
+        if maxCount < count:
+            maxCount = count
+
+if n == 1:
+    counts[1].append(nums[0])
+
+counts[maxCount].sort()
+
+if len(counts[maxCount]) == 1:
+    print(counts[maxCount][0])
+else:
+    print(counts[maxCount][1])
+
+print(nums[-1] - nums[0])
