ee9482272be1a4aa6d7071ed7de383afad7d83e6
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2023-07-19 00:20:10 +0900

    [Silver I] Title: 정수 삼각형, Time: 160 ms, Memory: 34044 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+tri = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(1, n):
+    for j in range(len(tri[i])):
+        if j == 0:
+            tri[i][j] += tri[i - 1][j]
+        elif j == i:
+            tri[i][j] += tri[i - 1][j - 1]
+        else:
+            tri[i][j] += max(tri[i - 1][j - 1], tri[i - 1][j])
+
+print(max(tri[-1]))
