037f3706e1b04322a4918ddeff57a0434e6f8f1b
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2023-09-18 00:33:10 +0900

    [unrated] Title: 과일 장수, Time: 0.35 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        Arrays.sort(score);
+        
+        int answer = 0;
+        int idx = score.length;
+        
+        while (idx > 0) {
+            idx -= m;
+            if (idx < 0) break;
+            answer += score[idx] * m;
+        }
+        
+        return answer;
+        
+    }
+}
