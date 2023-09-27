f4863d0fdced705ea6ede8bfcd59e52bf2e6fc13
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2023-09-27 18:22:13 +0900

    [unrated] Title: 대충 만든 자판, Time: 1.67 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.py
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.py
@@ -1,33 +1,20 @@
 def solution(keymap, targets):
-    map = getKeyMap(keymap)
+    map = {}
+    
+    for key in keymap:
+        for j in range(1, len(key) + 1):
+            map[key[j - 1]] = min(map.get(key[j - 1], 101), j)
 
     answer = []
 
     for target in targets:
         sum = 0
-        flag = True
         for x in target:
             if x not in map:
-                answer.append(-1)
-                flag = False
+                sum = -1
                 break
             else:
                 sum += map[x]
-        if flag:
-            answer.append(sum)
+        answer.append(sum)
 
     return answer
-
-
-def getKeyMap(keymap):
-    dic = dict()
-
-    for key in keymap:
-        for j in range(1, len(key) + 1):
-            dic[key[j - 1]] = min(dic.get(key[j - 1], 101), j)
-            # if key[j - 1] in dic:
-            #     dic[key[j - 1]] = min(dic[key[j - 1]], j)
-            # else:
-            #     dic[key[j - 1]] = j
-
-    return dic
