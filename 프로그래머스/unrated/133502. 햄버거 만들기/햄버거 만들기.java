f304c9ea0632f7fa7f26f09fa32a617a25133591
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2023-09-28 16:34:45 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
@@ -2,27 +2,30 @@ import java.util.*;
 
 class Solution {
     
-    Stack<Integer> stack = new Stack<>();
-    
     public int solution(int[] ingredient) {
         int answer = 0;
         
+        int[] stack = new int[ingredient.length];
+        int idx = 0;
+        
         for (int i = 0; i < ingredient.length; i++) {
-            stack.push(ingredient[i]);
-            if (stack.size() < 4) continue;
-            int a = stack.pop();
-            int b = stack.pop();
-            int c = stack.pop();
-            int d = stack.pop();
-            
-            if (a == 1 && b == 3 && c == 2 && d == 1) {
-                answer++;
-            } else {
-                stack.push(d);
-                stack.push(c);
-                stack.push(b);
-                stack.push(a);
+            stack[idx] = ingredient[i];
+            if (idx < 3) {
+                idx++;
+                continue;
             }
+            
+            int a = stack[idx - 3];
+            int b = stack[idx - 2];
+            int c = stack[idx - 1];
+            int d = stack[idx];
+            
+            if (a == 1 && b == 2 && c == 3 && d == 1) {
+                answer++;
+                idx -= 4;
+            }
+            
+            idx++;
             
         }
         
