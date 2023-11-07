c6316401227dc0c0e24c795c6477144fd59df5db
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2023-11-07 21:44:43 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    
+    Map<String, Integer> map;
+    
+    public int solution(String[][] clothes) {
+        
+        map = new HashMap<>();
+        
+        for (String[] arr : clothes) {
+            String item = arr[0];
+            String type = arr[1];
+            
+            map.put(type, map.getOrDefault(type, 0) + 1);
+        }
+        
+        int answer = 1;
+        for (int x : map.values()) {
+            answer *= (x + 1);
+        }
+        
+        answer--;        
+
+        return answer;
+    }
+}
