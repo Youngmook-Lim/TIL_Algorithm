41290af35efc364eafbefa1fdac67b42cda062d8
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2023-11-24 13:35:50 +0900

    [level 2] Title: 전화번호 목록, Time: 549.95 ms, Memory: 97.3 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,16 +2,12 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        
+                
         Arrays.sort(phone_book);
         
-        Set<String> set = new HashSet();
-        
-        for (int i = phone_book.length - 1; i >= 0; i--) {
-            String cur = phone_book[i];
-            if (set.contains(cur)) return false;
-            for (int j = 1; j <= cur.length(); j++) {
-                set.add(cur.substring(0, j));
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
+                return false;
             }
         }
         
