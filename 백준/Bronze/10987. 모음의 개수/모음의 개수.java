f54526cc1019ff6f29c1c2531206303f0983faf5
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-01-21 13:39:39 +0900

    [Bronze III] Title: 모음의 개수, Time: 84 ms, Memory: 11568 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        String input = br.readLine();
+        Set<Character> set = new HashSet<>();
+        set.add('a');
+        set.add('e');
+        set.add('i');
+        set.add('o');
+        set.add('u');
+
+        int cnt = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (set.contains(input.charAt(i))) {
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+
+        br.close();
+    }
+
+
+}
+
