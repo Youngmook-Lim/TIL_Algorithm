b01e9ae858281cc94f0f737c907615882aba3f32
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-02-22 22:41:33 +0900

    [level 3] Title: 기지국 설치, Time: 0.70 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -3,20 +3,12 @@ class Solution {
         
         int answer = 0;
         
-        for (int i = 0; i < stations.length; i++) {
-            int cur = stations[i] - 2 - w;
+        for (int i = 0; i <= stations.length; i++) {
+            int cur = i == stations.length ? n - 1 : stations[i] - 2 - w;
             int prev = i == 0 ? 0 : stations[i - 1] + w;
-            // System.out.println((cur - prev) / (w + 1));
             if (cur >= prev) {
                 answer += (cur - prev) / (2 * w + 1) + 1;                
             }
-        }
-        
-        int cur = n - 1;
-        int prev = stations[stations.length - 1] + w;
-        
-        if (cur >= prev) {
-            answer += (cur - prev) / (2 * w + 1) + 1;        
         }
 
         return answer;
