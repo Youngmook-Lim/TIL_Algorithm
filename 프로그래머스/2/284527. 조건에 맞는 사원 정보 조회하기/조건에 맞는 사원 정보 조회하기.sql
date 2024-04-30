2ce61412cd7c812d167c71eaae87083942ba3503
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-04-30 22:55:15 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,20 +1,14 @@
 -- 코드를 작성해주세요
-SELECT G.TOTAL_SCORE AS SCORE, 
-       E.EMP_NO,
-       E.EMP_NAME,
-       E.POSITION,
-       E.EMAIL
-FROM   HR_DEPARTMENT D,
-       HR_EMPLOYEES  E,
-       (SELECT *
-        FROM   (SELECT EMP_NO, 
-                SUM(SCORE) TOTAL_SCORE
-                FROM   HR_GRADE
-                GROUP BY EMP_NO) GG
-        WHERE  GG.TOTAL_SCORE = (SELECT MAX(GGG.TOTAL_SCORE)
-                                 FROM   (SELECT EMP_NO, 
-                                         SUM(SCORE) TOTAL_SCORE
-                                         FROM   HR_GRADE
-                                         GROUP BY EMP_NO) GGG)) G
-WHERE  D.DEPT_ID = E.DEPT_ID
-AND    E.EMP_NO  = G.EMP_NO;
+SELECT   SUM(SCORE) AS SCORE, 
+         E.EMP_NO,
+         E.EMP_NAME,
+         E.POSITION,
+         E.EMAIL
+FROM     HR_DEPARTMENT D,
+         HR_EMPLOYEES  E,
+         HR_GRADE      G
+WHERE    D.DEPT_ID = E.DEPT_ID
+AND      E.EMP_NO  = G.EMP_NO
+GROUP BY G.EMP_NO
+ORDER BY SCORE DESC
+LIMIT    1;
