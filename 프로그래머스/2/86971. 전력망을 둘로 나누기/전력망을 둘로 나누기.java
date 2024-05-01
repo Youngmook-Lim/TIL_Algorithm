d693b3c82d6b267ffcbba10542411dd174426b14
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-05-01 14:52:29 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.37 ms, Memory: 69.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,61 +1,30 @@
-import java.util.*;
 
 class Solution {
-    
-    boolean[] visited;
-    List<Integer>[] adj;
-    int[] sums;
-    int tmp;
-    
-    public int solution(int n, int[][] wires) {
-        
-        visited = new boolean[n + 1];
-        sums = new int[2];
-        adj = new List[n + 1];
-        for (int i = 1; i < n + 1; i++) {
-            adj[i] = new ArrayList<>();
-        }
-        for (int[] arr : wires) {
-            adj[arr[0]].add(arr[1]);
-            adj[arr[1]].add(arr[0]);
-        }
-        tmp = 0;
-        int cnt = 0;
-        int min = Integer.MAX_VALUE;
-        
-        for (int i = 0; i < n - 1; i++) {
-            // System.out.println("i = " + i);
-            visited = new boolean[n + 1];
-            int a = wires[i][0];
-            int b = wires[i][1];
-            adj[a].remove(Integer.valueOf(b));
-            adj[b].remove(Integer.valueOf(a));
-            cnt = 0;
-            for (int j = 1; j <= n; j++) {
-                if (!visited[j]) {
-                    tmp = 0;
-                    dfs(j);
-                    sums[cnt++] = tmp;
-                    if (cnt == 2) break;
-                }
+    int N, min;
+    int[][] map;
+    int[] vst;
+    int dfs(int n){
+        vst[n] = 1;
+        int child = 1;
+        for(int i = 1; i <= N; i++) {
+            if(vst[i] == 0 && map[n][i] == 1) {
+                vst[i] = 1;
+                child += dfs(i);
             }
-            adj[a].add(b);
-            adj[b].add(a);
-            min = Math.min(min, Math.abs(sums[1] - sums[0]));
-            // System.out.println("min is " + min);
         }
-        
-        return min;
+        min = Math.min(min, Math.abs(child - (N - child)));
+        return child;
     }
-    
-    void dfs(int node) {
-        // System.out.println(node);
-        tmp++;
-        visited[node] = true;
-        for (int x : adj[node]) {
-            if (!visited[x]) {
-                dfs(x);
-            }
+    public int solution(int n, int[][] wires) {
+        N = n;
+        min = n;
+        map = new int[n+1][n+1];
+        vst = new int[n+1];
+        for(int[] wire : wires) {
+            int a = wire[0], b = wire[1];
+            map[a][b] = map[b][a] = 1;
         }
+        dfs(1);
+        return min;
     }
 }
