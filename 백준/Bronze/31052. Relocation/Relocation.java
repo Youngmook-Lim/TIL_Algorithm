2dbe4f3cbe9c3f480f38c8693891f6f9ef54dca3
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-06-02 12:07:26 +0900

    [Bronze III] Title: Relocation, Time: 440 ms, Memory: 47292 KB -BaekjoonHub

---
+++ 백준/Bronze/31052. Relocation/Relocation.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < q; i++) {
+            st = new StringTokenizer(br.readLine());
+            int cmd = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (cmd == 1) {
+                arr[a - 1] = b;
+            } else {
+                sb.append(Math.abs(arr[a - 1] - arr[b - 1])).append('\n');
+            }
+        }
+
+        System.out.println(sb);
+
+        br.close();
+    }
+
+
+}
+
