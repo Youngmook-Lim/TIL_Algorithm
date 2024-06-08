5e027ce6eafeedc805c3769958a0ae12fc9747d4
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-06-08 20:54:08 +0900

    [level 2] Title: 호텔 대실, Time: 14.74 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -0,0 +1,75 @@
+import java.util.*;
+
+class Solution {
+    
+    public class Time implements Comparable<Time> {
+        int start, end;
+        
+        public Time(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }   
+        
+        @Override
+        public int compareTo(Time t) {
+            if (this.end != t.end) {
+                return t.end - this.end;
+            }
+            return t.start - this.start;
+        }
+        
+        @Override
+        public String toString() {
+            return (start / 60) + ":" + (start - (start / 60) * 60)  + " ~ " + (end / 60) + ":" + (end - (end / 60) * 60);
+        }
+    }
+    
+    public int solution(String[][] book_time) {
+        int answer = 0;
+        
+        List<Time> list = new ArrayList<>();
+        List<Time> res = new ArrayList<>();
+        
+        for (String[] arr : book_time) {
+            String start = arr[0];
+            String end = arr[1];
+            list.add(getNewTime(start, end));
+        }
+        
+        Collections.sort(list);   
+        
+        for (Time t : list) {
+            boolean flag = false;
+            for (int i = 0; i < res.size(); i++) {
+                Time r = res.get(i);
+                if (t.end + 10 <= r.start) {
+                    res.set(i, t);
+                    flag = true;
+                    // System.out.println("replaced : " + r + " with " + t);
+                    break;
+                }
+            }
+            
+            if (!flag) {
+                res.add(t);
+                // System.out.println("added : " + t);
+            }
+            
+        }
+        
+        return res.size();
+    }
+    
+    public Time getNewTime(String start, String end) {
+        int startHour = Integer.parseInt(start.substring(0, 2));
+        int startMin = Integer.parseInt(start.substring(3, 5));
+        int endHour = Integer.parseInt(end.substring(0, 2));
+        int endMin = Integer.parseInt(end.substring(3, 5));
+        
+        int startTotal = startHour * 60 + startMin;
+        int endTotal = endHour * 60 + endMin;
+        
+        return new Time(startTotal, endTotal);
+        
+    }
+}
